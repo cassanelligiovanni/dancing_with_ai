c64f061d3f2497c01293e92a1fdefd2d2d54a1a4
Giovanni Cassanelli <giovanni@Giovannis-Air.Home>
2021-10-01 00:56:16 +0200

    run name

--- train.py
+++ train.py
@@ -104,8 +104,8 @@ def train(maxEpochs, train_loader,  optimizer, criterion, device, encoder, decod
         train_log(current_loss, steps, epoch)
 
         if(epoch%500 == 0) :
-            torch.save(model.state_dict(), "model.h5")
-            wandb.save('model.h5')
+            torch.save(model.state_dict(), wandb.run.name +".h5")
+            wandb.save(wandb.run.name +".h5")
 
         if ((epoch+1)%10 == 0):
             test_log(model, data_dir, device)
